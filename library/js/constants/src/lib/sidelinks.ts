3bd9fa879008bbfc7095b02f35b7fbe9f957765e
rodentskie <rodentskie@gmail.com>
2024-11-08 14:41:32 +0800

    chore: active nav link

--- library/js/constants/src/lib/sidelinks.ts
+++ library/js/constants/src/lib/sidelinks.ts
@@ -5,10 +5,12 @@ export const infraLiveSideLinks: ISideLinkProps[] = [
     topic: 'p2-aus',
     subTopics: [
       {
+        key: 'certificates',
         label: 'Certificates',
         value: '/infra/live/p2-aus/certificates',
       },
       {
+        key: 'health-check',
         label: 'Health Check',
         value: '/infra/live/p2-aus/health-check',
       },
@@ -18,10 +20,12 @@ export const infraLiveSideLinks: ISideLinkProps[] = [
     topic: 'p2-euk',
     subTopics: [
       {
+        key: 'certificates',
         label: 'Certificates',
         value: '/infra/live/p2-euk/certificates',
       },
       {
+        key: 'health-check',
         label: 'Health Check',
         value: '/infra/live/p2-euk/health-check',
       },
@@ -31,10 +35,12 @@ export const infraLiveSideLinks: ISideLinkProps[] = [
     topic: 'p2-usa',
     subTopics: [
       {
+        key: 'certificates',
         label: 'Certificates',
         value: '/infra/live/p2-usa/certificates',
       },
       {
+        key: 'health-check',
         label: 'Health Check',
         value: '/infra/live/p2-usa/health-check',
       },
@@ -47,10 +53,12 @@ export const infraNonLiveSideLinks: ISideLinkProps[] = [
     topic: 'p2-sandbox',
     subTopics: [
       {
+        key: 'certificates',
         label: 'Certificates',
         value: '/infra/non-live/p2-sandbox/certificates',
       },
       {
+        key: 'health-check',
         label: 'Health Check',
         value: '/infra/non-live/p2-sandbox/health-check',
       },
@@ -60,10 +68,12 @@ export const infraNonLiveSideLinks: ISideLinkProps[] = [
     topic: 'p2-stage',
     subTopics: [
       {
+        key: 'certificates',
         label: 'Certificates',
         value: '/infra/non-live/p2-stage/certificates',
       },
       {
+        key: 'health-check',
         label: 'Health Check',
         value: '/infra/non-live/p2-stage/health-check',
       },
@@ -73,10 +83,12 @@ export const infraNonLiveSideLinks: ISideLinkProps[] = [
     topic: 'p2-prerelease',
     subTopics: [
       {
+        key: 'certificates',
         label: 'Certificates',
         value: '/infra/non-live/p2-prerelease/certificates',
       },
       {
+        key: 'health-check',
         label: 'Health Check',
         value: '/infra/non-live/p2-prerelease/health-check',
       },
