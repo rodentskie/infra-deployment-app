25662e450a36190d7c8abf1542a2bc126c5595f1
rodentskie <rodentskie@gmail.com>
2024-11-08 15:29:36 +0800

    chore: random themes

--- apps/main.app/theme.ts
+++ apps/main.app/theme.ts
@@ -1,6 +1,7 @@
 'use client';
 
-import { createTheme } from '@mantine/core';
+import { createTheme, rem } from '@mantine/core';
+import classes from './styles.module.css';
 
 export const theme = createTheme({
   breakpoints: {
@@ -9,5 +10,32 @@ export const theme = createTheme({
     md: '62em',
     lg: '75em',
     xl: '88em',
+  },
+  fontSizes: {
+    xs: rem(10),
+    sm: rem(11),
+    md: rem(14),
+    lg: rem(16),
+    xl: rem(20),
+  },
+  lineHeights: {
+    xs: '1.4',
+    sm: '1.45',
+    md: '1.55',
+    lg: '1.6',
+    xl: '1.65',
+  },
+  defaultGradient: {
+    from: 'orange',
+    to: 'red',
+    deg: 45,
+  },
+  defaultRadius: 'sm',
+  activeClassName: classes.active,
+  autoContrast: true,
+  luminanceThreshold: 0.36,
+  shadows: {
+    md: '1px 1px 3px rgba(0, 0, 0, .25)',
+    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
   },
 });
